09e5576173207a4ec69b1f7c398710bd7382afad
Yuşa Çapraz <computerhackfusion@gmail.com>
2023-08-23 12:41:16 +0300

    Unused imports removed

--- inventory-service/src/main/java/org/softtech/internship/backend/inventory/InventoryServiceApplication.java
+++ inventory-service/src/main/java/org/softtech/internship/backend/inventory/InventoryServiceApplication.java
@@ -1,20 +1,7 @@
 package org.softtech.internship.backend.inventory;
 
-import org.softtech.internship.backend.inventory.model.currency.Currency;
-import org.softtech.internship.backend.inventory.model.material.Material;
-import org.softtech.internship.backend.inventory.model.recipe.Recipe;
-import org.softtech.internship.backend.inventory.model.recipe_material.RecipeMaterial;
-import org.softtech.internship.backend.inventory.repository.CurrencyRepository;
-import org.softtech.internship.backend.inventory.repository.MaterialRepository;
-import org.softtech.internship.backend.inventory.repository.RecipeMaterialRepository;
-import org.softtech.internship.backend.inventory.repository.RecipeRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.math.BigDecimal;
-import java.util.List;
 
 @SpringBootApplication
 public class InventoryServiceApplication {
