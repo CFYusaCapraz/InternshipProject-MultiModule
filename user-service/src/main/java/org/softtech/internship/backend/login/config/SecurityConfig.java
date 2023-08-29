d29946bbeadab39a84a1e02f2666bebda0e3e1d0
Yuşa Çapraz <computerhackfusion@gmail.com>
2023-08-29 15:19:56 +0300

    Configuration annotation added

--- user-service/src/main/java/org/softtech/internship/backend/login/config/SecurityConfig.java
+++ user-service/src/main/java/org/softtech/internship/backend/login/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package org.softtech.internship.backend.login.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -11,6 +12,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 
+@Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
